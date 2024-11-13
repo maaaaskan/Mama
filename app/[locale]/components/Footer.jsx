4652e58903eac4d3a44e678f258eb68a5de81f8e
maaaaskan <maskantiktok@gmail.com>
2024-11-13 10:48:11 +0300

    Update Footer.jsx

--- app/[locale]/components/Footer.jsx
+++ app/[locale]/components/Footer.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { FaSnapchat } from "react-icons/fa";
+import { FaTelegram } from "react-icons/fa";
 
 export default function Footer() {
   return (
     <div className="flex justify-center items-center mt-16">
       <a href="https://snapchat.com/t/tFOzaxPy" target="_blank">
-        <FaSnapchat className="w-6 h-6 ltr:mr-3 rtl:ml-3 mb-4 cursor-pointer transition-all duration-200 ease-in-out hover:scale-125 focus:scale-125 active:scale-105" />
+        <FaTelegram className="w-6 h-6 ltr:mr-3 rtl:ml-3 mb-4 cursor-pointer transition-all duration-200 ease-in-out hover:scale-125 focus:scale-125 active:scale-105" />
       </a>
       <a
         href="https://www.instagram.com/maaaskan?igsh=M3p4ZzZ5YzBmNnhv&utm_source=qr"
