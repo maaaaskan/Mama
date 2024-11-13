3bb2c0df30d45d5bb77eeaca41738ceb5a52299e
maaaaskan <maskantiktok@gmail.com>
2024-11-13 11:28:11 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -13,6 +13,6 @@ module.exports = withNextIntl({
     return config;
   },
   images: {
-    domains: ['www.kurdstreet.com', 'http://www.shafaq.com' , 'www.gulanmedia.com'], // Add external domains here
+    domains: ['www.kurdstreet.com', 'www.shafaq.com' , 'www.gulanmedia.com'], // Add external domains here
   },
 });
