fb06e32eddeb5c94406e1718e6a2275d7eda4b75
maaaaskan <maskantiktok@gmail.com>
2024-11-13 10:54:34 +0300

    Update Main.jsx

--- app/[locale]/components/Main.jsx
+++ app/[locale]/components/Main.jsx
@@ -48,7 +48,7 @@ export default function Main() {
   };
 
   const boxes = [
-    { image: "/screnshoots/8.png", text: "بێکەس", link: "#" },
+    { image: "/screenshots/8.png", text: "بێکەس", link: "#" },
     { image: "https://kurdipedia.org/files/photos/2008/293.JPG?ver=20110917095358", text: "مەحوی", link: "#" },
     { image: "https://www.kdp.info/grafik/uploaded/2024/NB__2024_07_16_h16m19s4__BE.jpg", text: "نێچیرڤان بارزانی", link: "#" },
   ];
